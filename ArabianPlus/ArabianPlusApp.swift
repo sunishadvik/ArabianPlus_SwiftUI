cd4bc89f90690a8a16c948fd021615c958d8c1e3
Sunish Ram <sunish141991@gmail.com>
2024-07-29 20:49:17 +0530

    Initial Commit

---
+++ ArabianPlus/ArabianPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArabianPlusApp.swift
+//  ArabianPlus
+//
+//  Created by Sunish Ram on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ArabianPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArabianPlus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArabianPlus
+//
+//  Created by Sunish Ram on 29/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
